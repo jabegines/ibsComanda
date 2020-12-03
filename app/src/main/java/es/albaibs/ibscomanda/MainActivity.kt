510022a0da9a07e97aafcfeec64c5d046c26e5a0
Jose A. Begines <jabegines@gmail.com>
2020-12-03 20:37:57 +0100

    Inicio

--- app/src/main/java/es/albaibs/ibscomanda/MainActivity.kt
+++ app/src/main/java/es/albaibs/ibscomanda/MainActivity.kt
@@ -4,8 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inicializarControles()
     }
+
+
+    private fun inicializarControles() {
+        // Probando Git
+    }
+
 }
